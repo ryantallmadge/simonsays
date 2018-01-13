b734d3613860ffe31433fd9aed55c72ee8e3fdb4
Ryan Tallmadge <ryan.tallmadge@help.com>
2018-01-13 15:46:17 -0500

    Initial Commit

---
+++ Simon Says/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simon Says
+//
+//  Created by Ryan Tallmage on 1/13/18.
+//  Copyright © 2018 Ryan Tallmage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
